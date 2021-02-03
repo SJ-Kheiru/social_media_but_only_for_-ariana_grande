d478a718da79d680b35f8b223332cb399746246d
SJ-Kheiru <heiruamir@gmail.com>
2021-02-03 15:58:49 +0300

    First commit

---
+++ src/components/MessageCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Grid } from "@material/core";
+
+function App() {
+  return (
+    <Grid container>
+      <Grid>aome</Grid>
+    </Grid>
+  );
+}
+
+export default App;
